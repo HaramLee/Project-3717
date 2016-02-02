49877ba5ba86fd24399943bdf5323dc2fc4a2c07
Haram Lee <haram.lee@hotmail.ca>
2016-02-02 11:08:35 -0800

    Added setting feature to change color

--- ScheduleApp/app/src/main/java/com/example/haram/myapplication/setting_display.java
+++ ScheduleApp/app/src/main/java/com/example/haram/myapplication/setting_display.java
@@ -1,20 +1,43 @@
 package com.example.haram.myapplication;
 
+import android.content.Intent;
+import android.graphics.Color;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.Menu;
 import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.widget.CompoundButton;
+import android.widget.RadioGroup;
+import android.widget.RelativeLayout;
+import android.widget.Switch;
 
 /**
  * Created by Haram on 2016-02-01.
  */
 public class setting_display extends AppCompatActivity {
 
+    private RelativeLayout background;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.setting_page);
+
+        background = (RelativeLayout) findViewById(R.id.backgroundLayout);
+
+        Switch switch1 = (Switch) findViewById(R.id.switch1);
+        switch1.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+
+            @Override
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                if(isChecked) {
+                    background.setBackgroundColor(Color.BLUE);
+                } else {
+                    background.setBackgroundColor(Color.WHITE);
+                }
+            }
+        });
     }
 
     @Override
@@ -25,4 +48,19 @@ public class setting_display extends AppCompatActivity {
         inflater.inflate(R.menu.main_menu, menu);
         return true;
     }
+
+    public boolean onOptionsItemSelected(MenuItem item){
+        switch(item.getItemId()){
+            case R.id.settings_id:
+                Intent i = new Intent(setting_display.this, MainActivity.class);
+
+                startActivity(i);
+                break;
+            default:
+                break;
+        }
+        return true;
+    }
+
+
 }
