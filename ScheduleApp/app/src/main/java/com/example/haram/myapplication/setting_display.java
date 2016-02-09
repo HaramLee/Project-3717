65e4b1fc2fa50c2cb4580973199c31c3efd411af
Injo Baik <baikinjo.28@gmail.com>
2016-02-09 03:15:26 -0800

    made some modification on settings page by entering the user name and it returns with the user name I typed using linear layout from relative layout that haram created

--- ScheduleApp/app/src/main/java/com/example/haram/myapplication/setting_display.java
+++ ScheduleApp/app/src/main/java/com/example/haram/myapplication/setting_display.java
@@ -7,24 +7,29 @@ import android.support.v7.app.AppCompatActivity;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
+import android.view.View;
 import android.widget.CompoundButton;
+import android.widget.LinearLayout;
 import android.widget.RadioGroup;
 import android.widget.RelativeLayout;
 import android.widget.Switch;
+import android.widget.TextView;
 
 /**
  * Created by Haram on 2016-02-01.
  */
 public class setting_display extends AppCompatActivity {
 
-    private RelativeLayout background;
+    private LinearLayout background;
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.setting_page);
 
-        background = (RelativeLayout) findViewById(R.id.backgroundLayout);
+        background = (LinearLayout) findViewById(R.id.backgroundLayout);
 
         Switch switch1 = (Switch) findViewById(R.id.switch1);
         Switch sw_switch = (Switch) findViewById(R.id.sw_service);
@@ -75,5 +80,29 @@ public class setting_display extends AppCompatActivity {
         return true;
     }
 
+
+    public void onGetNameClick(View view) {
+
+        Intent getNameScreenIntent = new Intent(this, setting_displayTwo.class);
+
+        final int result = 1;
+
+        getNameScreenIntent.putExtra("callingActivity", "setting_display");
+
+        startActivityForResult(getNameScreenIntent, result);
+
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        TextView usersNameMessage = (TextView)
+                findViewById(R.id.users_name_message);
+
+        String nameSentBack = data.getStringExtra("UsersName");
+
+        usersNameMessage.append(" " + nameSentBack);
+    }
 
 }
