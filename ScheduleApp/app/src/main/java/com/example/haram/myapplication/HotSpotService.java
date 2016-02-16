7e66861bccdaf1ad87a3e25841e0a0dcb9ff9772
Dennis <dennis.chau@outlook.com>
2016-02-15 20:30:22 -0800

    improved hotspot fixed (without gesture)

--- ScheduleApp/app/src/main/java/com/example/haram/myapplication/HotSpotService.java
+++ ScheduleApp/app/src/main/java/com/example/haram/myapplication/HotSpotService.java
@@ -1,103 +1,73 @@
 package com.example.haram.myapplication;
 
 import android.app.Service;
-import android.content.Context;
-import android.graphics.Bitmap;
-import android.graphics.BitmapFactory;
-import android.graphics.Canvas;
-import android.graphics.Paint;
-import android.graphics.PixelFormat;
-import android.graphics.drawable.BitmapDrawable;
-import android.graphics.drawable.Drawable;
-import android.os.IBinder;
 import android.content.Intent;
-import android.os.Bundle;
+import android.graphics.PixelFormat;
+import android.os.IBinder;
 import android.util.DisplayMetrics;
-import android.util.Log;
 import android.view.Gravity;
-import android.view.MotionEvent;
-import android.view.ViewGroup;
+import android.view.View;
 import android.view.WindowManager;
-import android.widget.Button;
 import android.widget.RelativeLayout;
 import android.widget.TextView;
 import android.widget.Toast;
+//import com.example.haram.myapplication.EdgeDetector;
 
 public class HotSpotService extends Service {
-//    HUDView mView;
+//    EdgeDetector edgeDetector;
     RelativeLayout rl;
-    TextView tv;
-    WindowManager wm;
+    WindowManager windowManager;
 
-
-//    Bitmap hotspot = BitmapFactory.decodeResource(getResources(), R.drawable.ic_arrow_up_blue);
-
-    @Override
-    public IBinder onBind(Intent arg0) {
+    public IBinder onBind(Intent intent) {
         return null;
     }
 
     @Override
     public void onCreate() {
         super.onCreate();
-//        Toast.makeText(getBaseContext(), "Service onCreate", Toast.LENGTH_LONG).show();
-        wm = (WindowManager) getSystemService(WINDOW_SERVICE);
-        DisplayMetrics metrics = new DisplayMetrics();
-        wm.getDefaultDisplay().getMetrics(metrics);
-        int height = metrics.heightPixels;
+        DisplayMetrics displayMetrics = new DisplayMetrics();
+        windowManager = (WindowManager)getSystemService(WINDOW_SERVICE);
+        windowManager.getDefaultDisplay().getMetrics(displayMetrics);
+//        this.edgeDetector = new EdgeDetector();
 
+        int height = displayMetrics.heightPixels;
         rl = new RelativeLayout(this);
-        tv = new TextView(this);
+        TextView textView = new TextView(this);
 
-        tv.setText("FEEEEEEEEEEEEEEEED");
-        tv.setRotation(90);
-//        tv.setWidth((int) (height * 0.6));
-        tv.setSingleLine();
-        tv.setTextSize(16f);
-//        tv.setBackgroundResource(R.color.red);
+        textView.setSingleLine();
+        textView.setTextSize(16.0f);
+        textView.setText((CharSequence) " ");
+        textView.setHeight((int) (0.6 * height));
+        textView.setBackgroundResource(R.color.red);
 
-        RelativeLayout.LayoutParams RLParams = new RelativeLayout.LayoutParams(WindowManager.LayoutParams.WRAP_CONTENT,
-                                                                               WindowManager.LayoutParams.WRAP_CONTENT);
-
-//        RLParams.addRule(RelativeLayout.CENTER_VERTICAL);
-//        RLParams.addRule(RelativeLayout.ALIGN_PARENT_RIGHT);
-
-        rl.addView(tv, RLParams);
-
-        WindowManager.LayoutParams params = new WindowManager.LayoutParams();
-        params.height = WindowManager.LayoutParams.MATCH_PARENT;
-        params.width = WindowManager.LayoutParams.MATCH_PARENT;
-        params.type = WindowManager.LayoutParams.TYPE_SYSTEM_ALERT;
-//        params.gravity = Gravity.RIGHT;
-        params.flags = WindowManager.LayoutParams.FLAG_NOT_FOCUSABLE
-//                     | WindowManager.LayoutParams.FLAG_NOT_TOUCH_MODAL
-                     | WindowManager.LayoutParams.FLAG_WATCH_OUTSIDE_TOUCH;
-        params.format = PixelFormat.TRANSPARENT;
-//        params.gravity = Gravity.RIGHT;
-
-
-
-        wm.addView(rl, params);
-    }
-
-
-    @Override
-    public int onStartCommand(Intent intent, int flags, int startId) {
-        // Let it continue running until it is stopped.
-        Toast.makeText(this, "Service Started", Toast.LENGTH_LONG).show();
-        return START_STICKY;
+        RelativeLayout.LayoutParams layoutParams = new RelativeLayout.LayoutParams(RelativeLayout.LayoutParams.WRAP_CONTENT, RelativeLayout.LayoutParams.WRAP_CONTENT);;
+        rl.addView(textView, layoutParams);
+        WindowManager.LayoutParams layoutParams2 = new WindowManager.LayoutParams();
+        layoutParams2.height = WindowManager.LayoutParams.WRAP_CONTENT;
+        layoutParams2.width = WindowManager.LayoutParams.WRAP_CONTENT;;
+        layoutParams2.type = WindowManager.LayoutParams.TYPE_SYSTEM_ALERT;
+        layoutParams2.flags = WindowManager.LayoutParams.FLAG_WATCH_OUTSIDE_TOUCH | WindowManager.LayoutParams.FLAG_NOT_TOUCH_MODAL;
+        layoutParams2.format = PixelFormat.TRANSPARENT;
+        layoutParams2.gravity = Gravity.RIGHT;
+        windowManager.addView(rl, layoutParams2);
     }
 
     @Override
     public void onDestroy() {
         super.onDestroy();
-        Toast.makeText(this, "Service Destroyed", Toast.LENGTH_LONG).show();
+        Toast.makeText(this, "Service Destroyed", Toast.LENGTH_SHORT).show();
         if(rl != null)
         {
             ((WindowManager) getSystemService(WINDOW_SERVICE)).removeView(rl);
             rl = null;
         }
     }
-}
 
+    @Override
+    public int onStartCommand(Intent intent, int flags, int startId) {
+        // Let it continue running until it is stopped.
+        Toast.makeText(this, "Service Started", Toast.LENGTH_SHORT).show();
+        return START_STICKY;
+    }
+}
 
