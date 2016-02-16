ec3aaddd83dcab985f4b88af2a0326bee46df045
Dennis <dennis.chau@outlook.com>
2016-02-15 21:42:39 -0800

    popup code working

--- ScheduleApp/app/src/main/java/com/example/haram/myapplication/HotSpotService.java
+++ ScheduleApp/app/src/main/java/com/example/haram/myapplication/HotSpotService.java
@@ -5,9 +5,12 @@ import android.content.Intent;
 import android.graphics.PixelFormat;
 import android.os.IBinder;
 import android.util.DisplayMetrics;
+import android.view.GestureDetector;
 import android.view.Gravity;
+import android.view.MotionEvent;
 import android.view.View;
 import android.view.WindowManager;
+import android.widget.Button;
 import android.widget.RelativeLayout;
 import android.widget.TextView;
 import android.widget.Toast;
@@ -17,6 +20,10 @@ public class HotSpotService extends Service {
 //    EdgeDetector edgeDetector;
     RelativeLayout rl;
     WindowManager windowManager;
+    WindowManager windowManager2;
+    static int height;
+    static int width;
+    static TextView textView;
 
     public IBinder onBind(Intent intent) {
         return null;
@@ -27,20 +34,49 @@ public class HotSpotService extends Service {
         super.onCreate();
         DisplayMetrics displayMetrics = new DisplayMetrics();
         windowManager = (WindowManager)getSystemService(WINDOW_SERVICE);
+        windowManager2 = (WindowManager)getSystemService(WINDOW_SERVICE);
         windowManager.getDefaultDisplay().getMetrics(displayMetrics);
 //        this.edgeDetector = new EdgeDetector();
 
-        int height = displayMetrics.heightPixels;
+        height = displayMetrics.heightPixels;
+        width = displayMetrics.widthPixels;
         rl = new RelativeLayout(this);
-        TextView textView = new TextView(this);
+        textView = new TextView(this);
 
         textView.setSingleLine();
         textView.setTextSize(16.0f);
-        textView.setText((CharSequence) " ");
+        textView.setText("  ");
         textView.setHeight((int) (0.6 * height));
         textView.setBackgroundResource(R.color.red);
 
-        RelativeLayout.LayoutParams layoutParams = new RelativeLayout.LayoutParams(RelativeLayout.LayoutParams.WRAP_CONTENT, RelativeLayout.LayoutParams.WRAP_CONTENT);;
+        final Button button = new Button(this);
+
+        final WindowManager.LayoutParams layoutParams3 = new WindowManager.LayoutParams();
+        layoutParams3.height = WindowManager.LayoutParams.MATCH_PARENT;
+        layoutParams3.width = WindowManager.LayoutParams.MATCH_PARENT;
+        layoutParams3.type = WindowManager.LayoutParams.TYPE_SYSTEM_ALERT;
+        layoutParams3.flags = WindowManager.LayoutParams.FLAG_WATCH_OUTSIDE_TOUCH | WindowManager.LayoutParams.FLAG_NOT_TOUCH_MODAL;
+        layoutParams3.format = PixelFormat.TRANSPARENT;
+
+        textView.setOnTouchListener(new View.OnTouchListener() {
+            @Override
+            public boolean onTouch(View v, MotionEvent event) {
+                switch (event.getAction()) {
+                    case MotionEvent.ACTION_DOWN:
+                        textView.setBackgroundResource(R.color.white);
+                        windowManager2.addView(button, layoutParams3);
+                        break;
+                    case MotionEvent.ACTION_UP:
+                        textView.setBackgroundResource(R.color.red);
+                        windowManager2.removeView(button);
+                        break;
+
+                }
+                return true;
+            }
+        });
+
+        RelativeLayout.LayoutParams layoutParams = new RelativeLayout.LayoutParams(RelativeLayout.LayoutParams.WRAP_CONTENT, RelativeLayout.LayoutParams.WRAP_CONTENT);
         rl.addView(textView, layoutParams);
         WindowManager.LayoutParams layoutParams2 = new WindowManager.LayoutParams();
         layoutParams2.height = WindowManager.LayoutParams.WRAP_CONTENT;
@@ -50,6 +86,9 @@ public class HotSpotService extends Service {
         layoutParams2.format = PixelFormat.TRANSPARENT;
         layoutParams2.gravity = Gravity.RIGHT;
         windowManager.addView(rl, layoutParams2);
+
+
+
     }
 
     @Override
@@ -71,3 +110,15 @@ public class HotSpotService extends Service {
     }
 }
 
+class Gestures extends GestureDetector.SimpleOnGestureListener {
+
+    public boolean onScroll(MotionEvent e1, MotionEvent e2, float distanceX, float distanceY){
+        if (e1.getX() < HotSpotService.width) {
+            HotSpotService.textView.setBackgroundResource(R.color.black);
+            return true;
+        }
+        return false;
+
+    }
+
+}
